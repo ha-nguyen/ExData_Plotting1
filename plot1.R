5db94538dc13df633c2cb64c1fc29b8a7ad9a5d5
Ha Nguyen <hmnguy01@syr.edu>
2015-06-07 16:14:18 -0400

    Basic Plotting

---
+++ plot1.R
@@ -0,0 +1,37 @@
+# Author - Ha Nguyen
+# Course - Explaintory Data Analysis 
+# Project 1 - Analyze individual household electric power consumption Data Set
+## Examine how household energy usage varies over 2 day period in Feb - 2007
+
+# Getting data file 
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <- "/Users/akira_6686/Downloads/datasets/house_power_consump.zip"
+download.file(url = fileurl, destfile = destfile , method = "curl")
+unzip(zipfile = destfile, exdir = "/Users/akira_6686/Downloads/datasets/")
+
+# Loading package 
+library(data.table)
+power_consump <- fread("/Users/akira_6686/Downloads/datasets/household_power_consumption.txt", sep = ";", na.strings = "?")
+
+# Filter only two day: 1/2/2007 and 2/2/2007 
+power_consump.12 <- as.data.frame(power_consump[power_consump$Date %in% list("1/2/2007","2/2/2007")])
+
+# Make a new column to save Date and time
+power_consump.12$DateTime <- strptime(paste(power_consump.12$Date, power_consump.12$Time), format = "%d/%m/%Y %H:%M:%S")
+
+#Convert numeric columns to correct type 
+power_consump.12[, 3:9] <- as.data.frame(lapply(power_consump.12[,3:9], as.numeric))
+
+#Create graphs and save to PNG file 
+
+png("plot1.png", width = 480, height = 480)
+# Histogram to show data distribution of "Global Active Power" 
+hist(power_consump.12$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
+
+
+
+
+
+
+
